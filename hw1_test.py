96f16da831d9343c89d4c23cfd89a67dbf9ccdc4
Aaliyah <akelly47@calpoly.edu>
2024-10-21 13:36:25 -0700

    push 1

--- hw1_test.py
+++ hw1_test.py
@@ -2,7 +2,7 @@ import data
 import hw1
 import unittest
 
-from hw1 import add_prices, vowel_count
+from hw1 import add_prices, vowel_count, short_lists, ascending_pairs
 
 
 # Write your test cases for each part below.
@@ -22,21 +22,46 @@ class TestCases(unittest.TestCase):
         self.assertEqual(result, expected)
 
     # Part 2
+    def test_list(self):
+        lists = [[1, 2], [3, 4, 5], [6], [7, 8]]
+        result = short_lists(lists)
+        expected = [[1, 2], [7, 8]]
+        self.assertEqual(result, expected)
 
+    def test_list_2(self):
+        lists = [[1, 2], [3, 4], [5, 6]]
+        result = short_lists(lists)
+        expected = [[1, 2], [3, 4], [5, 6]]
+        self.assertEqual(result, expected)
 
     # Part 3
-
+    def test_ascending(self):
+        lists2 = [[4, 2], [3, 5, 1], [8], [9, 7]]
+        result = ascending_pairs(lists2)
+        expected = [[2, 4], [3, 5, 1], [8], [7, 9]]
+        self.assertEqual(result, expected)
+    def test_ascending_2(self):
+        lists2 = [[5, 3], [2, 4], [6, 1]]
+        result = ascending_pairs(lists2)
+        expected = [[3, 5], [2, 4], [1, 6]]
+        self.assertEqual(result, expected)
 
     # Part 4
 from hw1 import Price
-class TestAddPrice(unittest.TestCase):
-
+class TestAdd(unittest.TestCase):
     def test_adding(self):
-        price1 = Price(5,75)
-        price2 = Price(3, 20)
+        price1 = Price(5,50)
+        price2 = Price(3, 40)
         result = add_prices(price1, price2)
-        expected = Price(8, 95)
-        self.assertEqual(result, expected)
+        expected = Price(8, 90)
+        self.assertEqual((result.dollars, result.cents), (expected.dollars, expected.cents))
+
+    def test_adding_2(self):
+        price1 = Price(2, 80)
+        price2 = Price(3, 50)
+        result = add_prices(price1, price2)
+        expected = Price(6, 30)
+        self.assertEqual((result.dollars, result.cents), (expected.dollars, expected.cents))
 
 
     # Part 5
